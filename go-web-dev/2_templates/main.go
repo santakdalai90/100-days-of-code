c7ab4094b8738be4fecb5082adbdcf9924c36459
Santak Dalai <sdalai@truebool.com>
2019-10-08 00:22:11 +0530

    passing struct of slice of struct to the template

--- go-web-dev/2_templates/main.go
+++ go-web-dev/2_templates/main.go
@@ -18,44 +18,74 @@ func handleError(e error) {
 	}
 }
 
-type geo struct {
-	Capital string
+type Capital struct {
+	Name string
 	Country string
 	Currency string
 }
 
+type Company struct {
+	Name string
+	Product string
+}
+
 func main() {
-	Delhi := geo{
+	Delhi := Capital{
 		"Delhi",
 		"India",
 		"INR",
 	}
 
-	Moscow := geo{
+	Moscow := Capital{
 		"Moscow",
 		"Russia",
 		"RUB",
 	}
 
-	Beijing := geo{
+	Beijing := Capital{
 		"Beijing",
 		"China",
 		"CNY",
 	}
 
-	Tokyo := geo{
+	Tokyo := Capital{
 		"Tokyo",
 		"Japan",
 		"JPY",
 	}
 
-	Paris := geo{
+	Paris := Capital{
 		"Paris",
 		"France",
 		"EUR",
 	}
 
-	capitals := []geo{Delhi, Moscow, Beijing, Tokyo, Paris}
-	err := tpl.ExecuteTemplate(os.Stdout, "list_of_structs.gohtml", capitals)
+	capitals := []Capital{Delhi, Moscow, Beijing, Tokyo, Paris}
+
+	AsianPaint := Company{
+		Name:    "Asian Paints",
+		Product: "Paints",
+	}
+
+	TataSteel := Company{
+		Name:    "TATA Steel",
+		Product: "Steel",
+	}
+
+	MRF := Company{
+		Name:    "MRF",
+		Product: "Tyres",
+	}
+
+	companies := []Company{AsianPaint, TataSteel, MRF}
+
+	data := struct{
+		Capitals []Capital
+		Companies []Company
+	}{
+		capitals,
+		companies,
+	}
+	err := tpl.ExecuteTemplate(os.Stdout, "struct_list_struct.gohtml", data)
 	handleError(err)
 }
