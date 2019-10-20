68c3a8b56958be96f32cdf8f5382fcde3e4f40af
Santak Dalai <sdalai@truebool.com>
2019-10-21 01:51:23 +0530

    Learnt passing functions to template

--- go-web-dev/2_templates/main.go
+++ go-web-dev/2_templates/main.go
@@ -1,15 +1,70 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
 	"log"
+	"net/http"
 	"os"
+	"strings"
 	"text/template"
 )
 
 var tpl *template.Template
 
+func filterVowel(r rune) rune {
+	vowels := "aeiouAEIOU"
+	if strings.IndexRune(vowels, r) < 0 {
+		return r
+	}
+	return -1
+}
+
+func shortHand(input string) string {
+	// removes vowels from input string
+	return strings.Map(filterVowel, input)
+}
+
+func checkError(e error){
+	if e != nil {
+		fmt.Println(e.Error())
+	}
+}
+
+func getWeather(city string) string {
+	apiURL := "https://api.openweathermap.org/data/2.5/weather"
+	apiKey := "b784d5b15067cea930d3e00cdc0596e6"
+
+	req, _ := http.NewRequest(http.MethodGet, apiURL, nil)
+
+	queryParams := req.URL.Query()
+	queryParams.Add("q", city)
+	queryParams.Add("APPID", apiKey)
+
+	req.URL.RawQuery = queryParams.Encode()
+
+	client := http.Client{}
+	response, err := client.Do(req)
+	checkError(err)
+	body, _ := ioutil.ReadAll(response.Body)
+
+	var weatherData map[string]interface{}
+	err = json.Unmarshal(body, &weatherData)
+	checkError(err)
+
+	weather := weatherData["weather"].([]interface{})
+
+	return weather[0].(map[string]interface{})["main"].(string)
+}
+
+var UtiltyFunctionMap = template.FuncMap {
+	"shorthand":shortHand,
+	"weather": getWeather,
+}
+
 func init() {
-	tpl = template.Must(template.ParseGlob("templates/*"))
+	tpl = template.Must(template.New("").Funcs(UtiltyFunctionMap).ParseGlob("templates/*"))
 }
 
 func handleError(e error) {
@@ -30,31 +85,31 @@ type Company struct {
 }
 
 func main() {
-	Delhi := Capital{
+	Delhi := Capital {
 		"Delhi",
 		"India",
 		"INR",
 	}
 
-	Moscow := Capital{
+	Moscow := Capital {
 		"Moscow",
 		"Russia",
 		"RUB",
 	}
 
-	Beijing := Capital{
+	Beijing := Capital {
 		"Beijing",
 		"China",
 		"CNY",
 	}
 
-	Tokyo := Capital{
+	Tokyo := Capital {
 		"Tokyo",
 		"Japan",
 		"JPY",
 	}
 
-	Paris := Capital{
+	Paris := Capital {
 		"Paris",
 		"France",
 		"EUR",
@@ -62,30 +117,6 @@ func main() {
 
 	capitals := []Capital{Delhi, Moscow, Beijing, Tokyo, Paris}
 
-	AsianPaint := Company{
-		Name:    "Asian Paints",
-		Product: "Paints",
-	}
-
-	TataSteel := Company{
-		Name:    "TATA Steel",
-		Product: "Steel",
-	}
-
-	MRF := Company{
-		Name:    "MRF",
-		Product: "Tyres",
-	}
-
-	companies := []Company{AsianPaint, TataSteel, MRF}
-
-	data := struct{
-		Capitals []Capital
-		Companies []Company
-	}{
-		capitals,
-		companies,
-	}
-	err := tpl.ExecuteTemplate(os.Stdout, "struct_list_struct.gohtml", data)
+	err := tpl.ExecuteTemplate(os.Stdout, "functions.gohtml", capitals)
 	handleError(err)
 }
